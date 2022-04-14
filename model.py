85e003b36d9037a2d8c63d6a2d0d4c30ef1c530f
IndominousRex <aritrasaha33@gmail.com>
2022-04-14 12:17:08 +0530

    changes

--- model.py
+++ model.py
@@ -1,2 +1,6 @@
+from multiprocessing.spawn import import_main_path
 import numpy as np
 import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
