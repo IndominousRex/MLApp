6a977c0e6998843d72c7b80bf3a6f4941b656c4d
IndominousRex <aritrasaha33@gmail.com>
2022-04-16 14:34:54 +0530

    changes

--- app.py
+++ app.py
@@ -26,8 +26,33 @@ def predict():
     int_features[0, 0] = request.form.get("Distance driven")
     int_features[0, 1] = request.form.get("Age")
     int_features[0, 2] = request.form.get("Power")
-    d = {"First Owner": 3, "Second Owner": 5,
-         "Fourth Owner or more": 4, "Third Owner": 6, }
+    d = {"First Owner": 3,
+         "Second Owner": 5,
+         "Fourth Owner or more": 4,
+         "Third Owner": 6,
+         "BMW": 7,
+         "Bajaj": 8,
+         "Benelli": 9,
+         "Ducati": 10,
+         "Harley Davidson": 11,
+         "Hero": 12,
+         "Honda": 13,
+         "Hyosung": 14,
+         "Ideal": 15,
+         "Indian": 16,
+         "Jawa": 17,
+         "KTM": 18,
+         "Kawasaki": 19,
+         "LML": 20,
+         "MV": 21,
+         "Mahindra": 22,
+         "Rajdoot": 23,
+         "Royal Enfield": 24,
+         "Suzuki": 25,
+         "TVS": 26,
+         "Triumph": 27,
+         "Yamaha": 28,
+         "Yezdi": 29}
     o = request.form.get("Owner")
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
